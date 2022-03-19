543018672cece8b65d041106b9f130bd7cb6072d
Alexis DEVLEESCHAUWER <alexis@devleeschauwer.fr>
2022-03-19 13:41:07 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
 import logo from './logo.svg';
 import './App.css';
+import TabPanel from './components/TabPanel'
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+            {/*<header className="App-header">*/}
+            {/*    <p>*/}
+            {/*         Edit <code>src/App.tsx</code> and save to reload.*/}
+            {/*     </p>*/}
+            {/*     <a*/}
+            {/*        className="App-link"*/}
+            {/*        href="https://reactjs.org"*/}
+            {/*        target="_blank"*/}
+            {/*        rel="noopener noreferrer"*/}
+            {/*    >*/}
+            {/*        Learn React*/}
+            {/*    </a>*/}
+            {/*</header>*/}
+
+            <header>
+                <TabPanel />
             </header>
+            <body>
+                Alexis DEVLEESCHAUWER
+            </body>
+            <footer>
+
+            </footer>
+
         </div>
     );
 }
