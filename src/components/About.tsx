1f8cd8ea28f59a32e1c83bc5f80a118d9223c365
Alexis DEVLEESCHAUWER <alexis@devleeschauwer.fr>
2022-04-13 21:26:52 +0200

    added element in about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,39 +1,36 @@
-import React, { Fragment, memo } from 'react';
+import React, { memo } from 'react';
 
 import { useTranslation } from 'react-i18next';
 
-import { Typography } from '@mui/material';
+import { Avatar, Grid, Paper, Typography } from '@mui/material';
 
 import '../styles/App.css';
+import meImage from '../static/images/avatar/alexis.jpg';
 
 const About = () => {
     const { t } = useTranslation();
 
     return (
-        <Fragment>
-            <Typography align={'justify'}>
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-                {t('about.content')}
-            </Typography>
-        </Fragment>
+        <Grid container>
+            <Grid item xs={12} sm={4}>
+                <Typography variant="h6">{t('title.about')}</Typography>
+            </Grid>
+            <Grid item xs={0} sm={8} />
+
+            <Grid item xs={12} sm={2}>
+                <Avatar alt={`${t('me.firstname')} ${t('me.name')}`} src={meImage} sx={{ width: 96, height: 96 }} />
+            </Grid>
+            <Grid item xs={12} sm={10}>
+                <Paper elevation={3}>{t('about.content')}</Paper>
+            </Grid>
+
+            <Grid item>
+                <Typography align={'justify'}>
+                    <p>aaaaaaa</p>
+                    {t('about.content')}
+                </Typography>
+            </Grid>
+        </Grid>
     );
 };
 
